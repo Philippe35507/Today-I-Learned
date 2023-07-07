140873c823966daeccf59ec8bbc2c4cfc92136d5
Philippe35507 <phdefoy@gmail.com>
2023-07-08 00:13:51 +0100

    Creating the Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -52,33 +52,36 @@ const initialFacts = [
 */
 
 function App() {
-  // 1.Define state variable
   const [showForm, setShowForm] = useState(true);
 
-  const appTitle = "Today I Learned!";
   return (
     <>
-      {/* HEADER */}
-      <header className="header">
-        <div className="logo">
-          <img src="logo.png" alt="Today I leaned logo" />
-          <h1>{appTitle}</h1>
-        </div>
-        <button
-          className="btn btn-large btn-open"
-          // 2.Update state variable
-          onClick={() => setShowForm((show) => !show)}
-        >
-          Share a fact
-        </button>
-      </header>
-      {/* 2.Use state variable  */}
+      <Header showForm={showForm} setShowForm={setShowForm} />
+
       {showForm ? <NewFactForm /> : null}
       <main className="main">
         <CategoryFilter />
         <FactList />
       </main>
     </>
+  );
+}
+
+function Header({ showForm, setShowForm }) {
+  const appTitle = "Today I Learned!";
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" alt="Today I leaned logo" />
+        <h1>{appTitle}</h1>
+      </div>
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
   );
 }
 

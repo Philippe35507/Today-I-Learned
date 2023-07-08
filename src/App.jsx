fb17a71bfd8f1f374d5cb6d302868a5d3b58c826
Philippe35507 <phdefoy@gmail.com>
2023-07-08 14:16:15 +0100

    Adding a new fact Part 1 (error new Date)

--- src/App.jsx
+++ src/App.jsx
@@ -96,15 +96,45 @@ const CATEGORIES = [
   { name: "news", color: "#8b5cf6" },
 ];
 
+function isValidHttpUrl(string) {
+  let url;
+  try {
+    url = new URL(string);
+  } catch (_) {
+    return false;
+  }
+  return url.protocol === "http:" || url.protocol === "https:";
+}
+
 function NewFactForm() {
   const [text, setText] = useState("");
-  const [source, setSource] = useState("");
+  const [source, setSource] = useState("http://example.com");
   const [category, setCategory] = useState("");
   const textLength = text.length;
 
   function handleSubmit(e) {
+    // 1. Prevent browser reload
     e.preventDefault();
     console.log(text, source, category);
+
+    // 2. Check if the data is valid. If so, create a new fact.
+    if (text && isValidHttpUrl(source) && category && text.length <= 200) {
+      // 3. Create a new fact object
+      const newFact = {
+        id: Math.round(Math.random() * 1000000000),
+        text,
+        source,
+        category,
+        votesInteresting: 0,
+        votesMindblowing: 0,
+        votesFalse: 0,
+        createdIn: new Date().getCurrentYear(),
+      };
+
+      // 4. Add the new fact to state (to the user interface)
+      // 5. Reset the input field
+      // 6. Close the form
+    }
   }
   return (
     <form className="fact-form" onSubmit={handleSubmit}>
